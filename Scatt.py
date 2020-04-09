10a88324a75d3f37a1b89fcf3ecc46abc9e63fcc
Iamprashanth-1 <58625465+Iamprashanth-1@users.noreply.github.com>
2020-04-09 20:07:03 +0530

    placed scatt

---
+++ Scatt.py
@@ -0,0 +1,68 @@
+import plotly.graph_objects as go
+from collections import Counter
+import datetime
+import pandas as pd
+import plotly.express as px
+import dash_bootstrap_components as dbc
+fig7=go.Figure()
+
+dff= pd.read_csv('E:\customer29.csv')
+c=[]
+for kk in dff['Date received']:
+    c.append(kk)
+dd=dict(Counter(c))
+mm=list(dd.values())
+hg=list(dd.keys())
+#print(dd)
+pp=[]
+ee=[]
+for i in range(len(hg)):
+    a=[]
+    s=hg[i]
+    #print(s[1])
+    for j in range(len(s)):
+        if s[j]=='-':
+            a.append(int(s[:j]))
+            if s[j]==s[j+2]:
+                a.append(int(s[j+1:j+2]))
+            else:
+                a.append(int(s[j+1:j+3]))
+            break
+    #print(a)
+    aa=int(s[-4:])
+    a.append(aa)      
+    #print(a)
+    e=str(a[2])+'-'+str(a[0])+'-'+str(a[1])
+    ee.append(e)
+    pp.append(datetime.datetime(year=a[2], month=a[0], day=a[1]))
+#print(ee)
+dif=dict()
+sif=[]
+dif['Date']=ee
+dif['compliants']=mm
+sif.append(dif)
+#print(dif)
+
+
+fig7 =px.line(dif,x='Date',y='compliants',title='Time Series with Range Slider and Selectors')
+
+fig7.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+fig7.update_layout(
+    height=600,
+    template="plotly_dark",
+    
+)
+fig7.update_xaxes(rangeslider_visible=True)
--- app.py
+++ app.py
@@ -26,6 +26,7 @@ from geoloc import fig2
 from geoloc_satellite_map import fig3
 from dudo import fig5
 from updatedgeo import fig9
+from Scatt import fig7
 
 
 DATA_PATH = pathlib.Path(__file__).parent.resolve()
@@ -604,6 +605,10 @@ PIE_GRAPH_FASTAG=[
     dbc.CardHeader(html.H5("RESOLVED ISSUES WITHIN 48hrs and HOW THEY ARE SUBMITTED")),
     dcc.Graph(figure=fig5)
     ]
+SCATTER_GRAPH=[
+    dbc.CardHeader(html.H5("DAILY COMPLIANTS")),
+    dcc.Graph(figure=fig7)
+    ]
 sentiment_colors = {-1:"#EE6055",
                     -0.5:"#FDE74C",
                      0:"#FFE6AC",
@@ -624,6 +629,7 @@ BODY = dbc.Container(
         dbc.Card(WORDCLOUD_PLOTS),
         dbc.Row([dbc.Col([dbc.Card(LDA_PLOTS)])], style={'marginTop':30}),
         dbc.Card(PIE_GRAPH_FASTAG),
+        dbc.Card(SCATTER_GRAPH),
         dbc.Row([dbc.Col([dbc.Card(CUSTOMER_LOCATION_PLOT)]),dbc.Col([dbc.Card(CUSTOMER_LOCATION_MAP)])]),
         #dbc.Card(PIE_GRAPH_FASTAG),
        # dbc.Card(CUSTOMER_LOCATION_MAP),
